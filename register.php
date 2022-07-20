e6fdf942225d3c5891c03d0d99f5615986ebe35a
Ghanshyam S Nair <91174790+GhanShyam-nair@users.noreply.github.com>
2022-07-21 01:38:46 +0530

    first commit

---
+++ register.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title> Hello!</title>
+    <head>
+<body>
+    <div id="inputContiner">
+    <form id="loginform" action="register.php" method="POST">
+        <h2>LOGIN TO YOUR ACCOUNT</h2>
+        <p>
+        <label for="loginUsername">Username</label>
+        <input id="loginUsername" name="loginUsername" type="text" placeholder="eg. bart simpson" required>
+        </p>
+        <p>
+        <label for="loginPassword">Password</label>
+        <input id="loginPassword" name="loginPassword" type="password">
+        </p>
+        <button type="submit" name="loginButton">LOG IN</button>
+    </form>
+    </div>
+</body>
+</html>
